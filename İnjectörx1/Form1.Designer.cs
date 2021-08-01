17fd41643646d21ebd1fdc2830a0da447f486a81
umittasdemirx1 <88289427+umittasdemir2003@users.noreply.github.com>
2021-08-01 22:36:20 +0300

    C# Injector

---
+++ İnjectörx1/Form1.Designer.cs
@@ -0,0 +1,239 @@
+﻿
+namespace İnjectörx1
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.PnameT = new System.Windows.Forms.TextBox();
+            this.DllPathT = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.chAutoInject = new System.Windows.Forms.CheckBox();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.statusT = new System.Windows.Forms.TextBox();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.label3 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(12, 30);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(88, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Process Name";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.Location = new System.Drawing.Point(48, 55);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(52, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Dll Path";
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(103, 81);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(108, 95);
+            this.listBox1.TabIndex = 2;
+            this.listBox1.DoubleClick += new System.EventHandler(this.listBox1_DoubleClick);
+            // 
+            // PnameT
+            // 
+            this.PnameT.Location = new System.Drawing.Point(103, 28);
+            this.PnameT.Name = "PnameT";
+            this.PnameT.Size = new System.Drawing.Size(199, 20);
+            this.PnameT.TabIndex = 3;
+            // 
+            // DllPathT
+            // 
+            this.DllPathT.Location = new System.Drawing.Point(103, 55);
+            this.DllPathT.Name = "DllPathT";
+            this.DllPathT.Size = new System.Drawing.Size(199, 20);
+            this.DllPathT.TabIndex = 4;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button1.Location = new System.Drawing.Point(309, 51);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(30, 23);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "...";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(217, 81);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(98, 23);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "Show Processes";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button3.Location = new System.Drawing.Point(152, 182);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(59, 23);
+            this.button3.TabIndex = 7;
+            this.button3.Text = "İnject";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.BackColor = System.Drawing.Color.Transparent;
+            this.groupBox1.Controls.Add(this.chAutoInject);
+            this.groupBox1.Controls.Add(this.checkBox1);
+            this.groupBox1.Location = new System.Drawing.Point(217, 110);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(98, 66);
+            this.groupBox1.TabIndex = 8;
+            this.groupBox1.TabStop = false;
+            // 
+            // chAutoInject
+            // 
+            this.chAutoInject.AutoSize = true;
+            this.chAutoInject.Location = new System.Drawing.Point(7, 35);
+            this.chAutoInject.Name = "chAutoInject";
+            this.chAutoInject.Size = new System.Drawing.Size(77, 17);
+            this.chAutoInject.TabIndex = 1;
+            this.chAutoInject.Text = "Auto İnject";
+            this.chAutoInject.UseVisualStyleBackColor = true;
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(7, 12);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(59, 17);
+            this.checkBox1.TabIndex = 0;
+            this.checkBox1.Text = "Mode2";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // timer2
+            // 
+            this.timer2.Enabled = true;
+            this.timer2.Interval = 1000;
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
+            // 
+            // statusT
+            // 
+            this.statusT.Location = new System.Drawing.Point(215, 179);
+            this.statusT.Name = "statusT";
+            this.statusT.Size = new System.Drawing.Size(100, 20);
+            this.statusT.TabIndex = 9;
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(123, 9);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(120, 13);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "YouTube: By X_Hooker";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(351, 214);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.statusT);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.DllPathT);
+            this.Controls.Add(this.PnameT);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "İnjectör";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.TextBox PnameT;
+        private System.Windows.Forms.TextBox DllPathT;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.CheckBox chAutoInject;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Timer timer2;
+        private System.Windows.Forms.TextBox statusT;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ İnjectörx1/Form1.cs
@@ -0,0 +1,200 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+using System.Diagnostics;
+using System.IO;
+
+namespace İnjectörx1
+{
+    public partial class Form1 : Form
+    {
+        static readonly IntPtr INTPTR_ZERO = (IntPtr)0;
+
+        [DllImport("kernel32.dll", SetLastError = true)]
+        static extern IntPtr OpenProcess(uint dwDesiredAccess, int bInherİtHandle, uint dwProcessId);
+
+        [DllImport("kernel32.dll", SetLastError = true)]
+        static extern int CloseHandle(IntPtr hObject);
+
+        [DllImport("kernel32.dll", SetLastError = true)]
+        static extern IntPtr GetProcAddress(IntPtr Hmodule, string lpProcName);
+        [DllImport("kernel32.dll", SetLastError = true)]
+        static extern IntPtr VirtualAllocEx(IntPtr hProcess, IntPtr lpAddress, IntPtr dwsize, uint flAllocationType, uint flProtect);
+        [DllImport("kernel32.dll", SetLastError = true)]
+        static extern int WriteProcessMemory(IntPtr hProcces, IntPtr lpBaseAddres, byte[] buffer, uint size, int lpNumberOfBytesWritten);
+        [DllImport("kernel32.dll", SetLastError = true)]
+        static extern IntPtr CreateRemoteThread(IntPtr hProcess, IntPtr lpThreadAttribute, IntPtr dwStackSize, IntPtr lpStartAddres,
+            IntPtr lpParameter, uint dwCreationFlags, IntPtr lpThreadId);
+       
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public enum DllInjectionResult
+        {
+            DllNotFound,
+            GameProcessNotFound,
+            InjectionFailed,
+            Success
+        }
+
+        public DllInjectionResult Inject(String sProcName,string sDLLPath)
+        {
+            if (!File.Exists(sDLLPath))
+            {
+                return DllInjectionResult.DllNotFound;
+            }
+
+            uint _procId = 0;
+
+            Process[] _procs = Process.GetProcesses();
+            for (int i = 0; i < _procs.Length; i++)
+            {
+                if (_procs[i].ProcessName == sProcName)
+                {
+                    _procId = (uint)_procs[i].Id;
+                    break;
+                }
+
+            }
+
+            if (_procId == 0)
+            {
+                return DllInjectionResult.GameProcessNotFound;
+            }
+
+            if (bInject(_procId, sDLLPath))
+            {
+                return DllInjectionResult.InjectionFailed;
+            }
+
+            return DllInjectionResult.Success;
+
+        }
+
+        bool bInject(uint pToBeInjected, string sDllPath)
+        {
+            IntPtr hnPro = OpenProcess((0x2 | 0x8 | 0x10 | 0x20 | 0x400), 1, pToBeInjected);
+
+            if (hnPro == INTPTR_ZERO)
+            {
+                return false;
+            }
+
+            IntPtr lpAddress = VirtualAllocEx(hnPro, (IntPtr)null, (IntPtr)sDllPath.Length, (0x1000 | 0x2000), 0x40);
+
+            if (lpAddress == INTPTR_ZERO)
+            {
+                return false;
+            }
+
+            byte[] bytes = Encoding.ASCII.GetBytes(sDllPath);
+
+            if (WriteProcessMemory(hnPro, lpAddress, bytes, (uint)bytes.Length, 0)== 0)
+            {
+                return false;
+            }
+
+            if (CreateRemoteThread(hnPro, (IntPtr)null, INTPTR_ZERO, lpAddress, lpAddress, 0, (IntPtr)null)== INTPTR_ZERO)
+            {
+                return false;
+            }
+
+            CloseHandle(hnPro);
+            return true;
+        }
+
+        private void injectMode()
+        {
+            if (checkBox1.Checked)
+            {
+                Process[] ProcessCollection = Process.GetProcesses();
+                foreach (Process p in ProcessCollection)
+                {
+                    if (p.ProcessName == PnameT.Text.ToString() && p.MainWindowTitle != "")
+                    {
+                        Inject(PnameT.Text, DllPathT.Text);
+                        Console.Beep();
+                        chAutoInject.Checked = false;
+                        timer1.Stop();
+                        break;
+
+                    }
+                } 
+            }
+            else
+            {
+                Process[] processCollection = Process.GetProcesses();
+                foreach(Process p in processCollection)
+                {
+                    if (p.ProcessName == PnameT.Text.ToString())
+                    {
+                        Inject(PnameT.Text, DllPathT.Text);
+                        Console.Beep();
+                        chAutoInject.Checked = false;
+                        timer1.Stop();
+                        break;
+                    }
+                }
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            injectMode();
+        }
+
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+            if (chAutoInject.Checked)
+            {
+                timer1.Start();
+                return;
+            }
+            timer1.Stop();
+        }
+
+        private void listBox1_DoubleClick(object sender, EventArgs e)
+        {
+            if (listBox1.Items.Count !=0)
+            {
+                PnameT.Text = listBox1.SelectedItem.ToString();
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            statusT.Clear();
+            injectMode();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+            Process[] processes = Process.GetProcesses();
+            for (int i = 0; i < processes.Length; i ++)
+            {
+                Process p = processes[i];
+                listBox1.Items.Add(p.ProcessName);
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            openFileDialog1 = new OpenFileDialog();
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                DllPathT.Text = openFileDialog1.FileName;
+            }
+        }
+    }
+}
